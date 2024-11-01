6e21abea457f3082359840b9d318cc45226e7827
Kang Hyun Woo <gusdn741021@gmail.com>
2024-11-01 10:03:57 +0900

    first commit

---
+++ src/main/java/com/wekids/backend/WekidsApplication.java
@@ -0,0 +1,13 @@
+package com.wekids.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WekidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WekidsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wekids/backend/WekidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wekids.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WekidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
