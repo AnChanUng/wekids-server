027d1815182993f12fabe6049183811affb39a78
Kang Hyun Woo <gusdn741021@gmail.com>
2024-11-04 14:39:03 +0900

    [refact/#13]: @EqualsAndHashCode 적용
     - Lombok를 활용하여 코드 리팩토링

--- src/main/java/com/wekids/backend/member/domain/mapping/ParentChildId.java
+++ src/main/java/com/wekids/backend/member/domain/mapping/ParentChildId.java
@@ -2,28 +2,14 @@ package com.wekids.backend.member.domain.mapping;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-
 import java.io.Serializable;
+import lombok.EqualsAndHashCode;
 
-import java.util.Objects;
 
 @NoArgsConstructor
 @AllArgsConstructor
-public class ParentChildId  implements Serializable {
+@EqualsAndHashCode
+public class ParentChildId implements Serializable {
     private Long parent;
     private Long child;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ParentChildId that = (ParentChildId) o;
-        return Objects.equals(parent, that.parent) && Objects.equals(child, that.child);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(parent, child);
-    }
-
 }
