c3f6bccb0f9a17b3af2a46b230f5bfbdcd2e2f10
yoon <cbj0010@naver.com>
2024-11-08 16:04:50 +0900

    [feat/#8]: 거래아이디로 조회 service구현

--- src/main/java/com/wekids/backend/accountTransaction/service/AccountTransactionService.java
+++ src/main/java/com/wekids/backend/accountTransaction/service/AccountTransactionService.java
@@ -1,4 +1,7 @@
 package com.wekids.backend.accountTransaction.service;
 
+import com.wekids.backend.accountTransaction.domain.AccountTransaction;
+
 public interface AccountTransactionService {
+    AccountTransaction findByTransactionId(Long transactionId);
 }
--- src/main/java/com/wekids/backend/accountTransaction/service/AccountTransactionServiceImpl.java
+++ src/main/java/com/wekids/backend/accountTransaction/service/AccountTransactionServiceImpl.java
@@ -1,5 +1,7 @@
 package com.wekids.backend.accountTransaction.service;
 
+import com.wekids.backend.accountTransaction.domain.AccountTransaction;
+import com.wekids.backend.accountTransaction.repository.AccountTransactionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -9,5 +11,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Slf4j
-public class AccountTransactionServiceImpl implements AccountTransactionService{
+public class AccountTransactionServiceImpl implements AccountTransactionService {
+
+    private final AccountTransactionRepository accountTransactionRepository;
+
+    @Override
+    public AccountTransaction findByTransactionId(Long transactionId) {
+        return accountTransactionRepository.findByTransactionId(transactionId);
+    }
 }
