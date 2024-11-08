123af2f8df14a8024f0c0ed4fb5e4e1b92bf64c5
jabin1221 <koreanjb1221@gmail.com>
2024-11-08 17:52:50 +0900

    [feat/#15]: MemberController 작업

--- src/main/java/com/wekids/backend/member/controller/MemberController.java
+++ src/main/java/com/wekids/backend/member/controller/MemberController.java
@@ -1,6 +1,11 @@
 package com.wekids.backend.member.controller;
 
+import com.wekids.backend.member.dto.request.ParentAccountRequest;
+import com.wekids.backend.member.dto.response.ParentAccountResponse;
+import com.wekids.backend.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/api/v1/parents")
+    public void getParent_Account(@RequestBody ParentAccountRequest parentAccountRequest){
+
+    }
 }
