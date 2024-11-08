72859926fe6ca523b4ac33bb06bed21e0e996833
yoon <cbj0010@naver.com>
2024-11-08 16:05:25 +0900

    [feat/#8]: 거래아이디로 조회 repository 생성

--- src/main/java/com/wekids/backend/accountTransaction/repository/AccountTransactionRepository.java
+++ src/main/java/com/wekids/backend/accountTransaction/repository/AccountTransactionRepository.java
@@ -3,7 +3,11 @@ package com.wekids.backend.accountTransaction.repository;
 import com.wekids.backend.accountTransaction.domain.AccountTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Repository
 public interface AccountTransactionRepository extends JpaRepository<AccountTransaction,Long> {
+
+AccountTransaction findByTransactionId(Long transactionId);
+
 }
