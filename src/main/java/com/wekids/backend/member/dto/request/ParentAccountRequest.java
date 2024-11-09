a486122c0d9dca1ec206ba7acfe2fec63885f3ba
jabin1221 <koreanjb1221@gmail.com>
2024-11-09 12:43:01 +0900

    [feat/#15]: 테스트 코드 작성

--- src/main/java/com/wekids/backend/member/dto/request/ParentAccountRequest.java
+++ src/main/java/com/wekids/backend/member/dto/request/ParentAccountRequest.java
@@ -1,8 +1,14 @@
 package com.wekids.backend.member.dto.request;
 
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 public class ParentAccountRequest {
     Long parentId;
+
+    public ParentAccountRequest(Long parentId){
+        this.parentId = parentId;
+    }
 }
--- src/test/java/com/wekids/backend/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/wekids/backend/member/repository/MemberRepositoryTest.java
@@ -1,22 +1,40 @@
 package com.wekids.backend.member.repository;
 
+import com.wekids.backend.account.domain.enums.AccountDesignType;
 import com.wekids.backend.member.domain.Member;
+import com.wekids.backend.member.dto.request.ParentAccountRequest;
+import com.wekids.backend.member.dto.response.ParentAccountResponse;
+import com.wekids.backend.member.service.MemberService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class MemberRepositoryTest {
+    @Autowired
+    private MemberService memberService;
     @Autowired
     MemberRepository memberRepository;
     @Test
     void select() {
         List<Member> all = memberRepository.findAll();
         System.out.println("all = " + all);
+    }
+    @Test
+    void 부모_정보_가져오기(){
+        ParentAccountRequest request = new ParentAccountRequest(1L);
+        ParentAccountResponse response = memberService.getParent_Account(request);
+        assertThat(response.getAccountNumber()).isEqualTo("123-456-7890");
+        assertThat(response.getProfile()).isEqualTo("profile1.jpg");
+        assertThat(response.getBalance()).isEqualByComparingTo("100000.00");
+        assertThat(response.getDesignType()).isEqualTo(AccountDesignType.WEBEE);
+        assertThat(response.getAccountId()).isEqualTo(1);
+        assertThat(response.getChildren()).hasSize(2);
     }
 
 }
