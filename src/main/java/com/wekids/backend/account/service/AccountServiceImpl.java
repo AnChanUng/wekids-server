4ef791d6e44a0bcb09ec3923f33345a5df652c06
jabin1221 <koreanjb1221@gmail.com>
2024-11-11 16:23:43 +0900

    [feat/#5]: errorcode 수정

--- src/main/java/com/wekids/backend/account/service/AccountServiceImpl.java
+++ src/main/java/com/wekids/backend/account/service/AccountServiceImpl.java
@@ -22,7 +22,7 @@ public class AccountServiceImpl implements AccountService{
     public AccountResponse getAccount(Long account_id) {
         //AccountRepo에서 계좌 조회
         Account account = accountRepository.findById(account_id)
-                .orElseThrow(() -> new WekidsException(ErrorCode.MEMBER_NOT_FOUND, "계좌를 찾을 수 없습니다."));
+                .orElseThrow(() -> new WekidsException(ErrorCode.ACCOUNT_NOT_FOUND, "계좌를 찾을 수 없습니다."));
         return new AccountResponse(account);
     }
 }
