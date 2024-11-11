a16c25420d28e6d05205c93beb3798cf8282d0e0
jabin1221 <koreanjb1221@gmail.com>
2024-11-11 16:27:52 +0900

    [feat/#5]: test code 작성

--- src/test/java/com/wekids/backend/account/repository/AccountRepositoryTest.java
+++ src/test/java/com/wekids/backend/account/repository/AccountRepositoryTest.java
@@ -1,22 +1,41 @@
 package com.wekids.backend.account.repository;
 
 import com.wekids.backend.account.domain.Account;
+import com.wekids.backend.account.domain.enums.AccountDesignType;
+import com.wekids.backend.account.domain.enums.AccountState;
+import com.wekids.backend.account.dto.response.AccountResponse;
+import com.wekids.backend.account.service.AccountService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class AccountRepositoryTest {
     @Autowired
     AccountRepository accountRepository;
+    @Autowired
+    AccountService accountService;
 
     @Test
     void test() {
         List<Account> all = accountRepository.findAll();
         System.out.println("all = " + all);
+    }
+
+    @Test
+    void 계좌_가져오기(){
+        AccountResponse response = accountService.getAccount(1L);
+        assertThat(response.getId()).isEqualTo(1);
+        assertThat(response.getName()).isEqualTo("김철수");
+        assertThat(response.getAmount()).isEqualTo("100000.00");
+        assertThat(response.getState()).isEqualTo(AccountState.ACTIVE);
+        assertThat(response.getDesign_type()).isEqualTo(AccountDesignType.WEBEE);
+        assertThat(response.getAccount_number()).isEqualTo("123-456-7890");
+
     }
 }
