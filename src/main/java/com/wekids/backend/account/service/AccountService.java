dbb36066454b8b24b8342338a07bff2a199cbf8c
jabin1221 <koreanjb1221@gmail.com>
2024-11-11 16:09:12 +0900

    [feat/#5]: Service 세팅

--- src/main/java/com/wekids/backend/account/service/AccountService.java
+++ src/main/java/com/wekids/backend/account/service/AccountService.java
@@ -1,4 +1,8 @@
 package com.wekids.backend.account.service;
 
+import com.wekids.backend.account.dto.response.AccountResponse;
+
 public interface AccountService {
+
+    AccountResponse getAccount(Long account_id);
 }
--- src/main/java/com/wekids/backend/account/service/AccountServiceImpl.java
+++ src/main/java/com/wekids/backend/account/service/AccountServiceImpl.java
@@ -1,5 +1,10 @@
 package com.wekids.backend.account.service;
 
+import com.wekids.backend.account.domain.Account;
+import com.wekids.backend.account.dto.response.AccountResponse;
+import com.wekids.backend.account.repository.AccountRepository;
+import com.wekids.backend.exception.ErrorCode;
+import com.wekids.backend.exception.WekidsException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,4 +15,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Slf4j
 public class AccountServiceImpl implements AccountService{
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public AccountResponse getAccount(Long account_id) {
+
+        return null;
+    }
 }
