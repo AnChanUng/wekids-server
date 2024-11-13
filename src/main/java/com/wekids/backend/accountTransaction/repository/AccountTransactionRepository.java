76fd5dc74ccb8f81417ddbbf157dbd40e751816c
yoon <cbj0010@naver.com>
2024-11-13 18:18:38 +0900

    [feat/#8]: Id로 찾기
    jpa가 제공해주는 findById로 찾기

--- src/main/java/com/wekids/backend/accountTransaction/repository/AccountTransactionRepository.java
+++ src/main/java/com/wekids/backend/accountTransaction/repository/AccountTransactionRepository.java
@@ -1,13 +1,9 @@
 package com.wekids.backend.accountTransaction.repository;
 
 import com.wekids.backend.accountTransaction.domain.AccountTransaction;
+import com.wekids.backend.accountTransaction.dto.response.TransactionDetailSearchResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Repository
-public interface AccountTransactionRepository extends JpaRepository<AccountTransaction,Long> {
-
-AccountTransaction findByTransactionId(Long transactionId);
-
+public interface AccountTransactionRepository extends JpaRepository<AccountTransaction, Long> {
 }
