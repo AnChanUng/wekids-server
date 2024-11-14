90b8df57a775563f69a26b37e22188fabdf7d48d
jabin1221 <koreanjb1221@gmail.com>
2024-11-14 11:11:30 +0900

    [feat/#7]: JPA문 작성

--- src/main/java/com/wekids/backend/accountTransaction/repository/AccountTransactionRepository.java
+++ src/main/java/com/wekids/backend/accountTransaction/repository/AccountTransactionRepository.java
@@ -1,9 +1,18 @@
 package com.wekids.backend.accountTransaction.repository;
 
 import com.wekids.backend.accountTransaction.domain.AccountTransaction;
+import com.wekids.backend.accountTransaction.domain.enums.TransactionType;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface AccountTransactionRepository extends JpaRepository<AccountTransaction,Long> {
+
+    List<AccountTransaction> findBySenderOrReceiverAndCreatedAtBetweenAndType(
+            String sender, String receiver, LocalDateTime startTime, LocalDateTime endTime, TransactionType type, Pageable pageable);
+    Long countBySenderOrReceiverAndCreatedAtBetweenAndType(String sender, String receiver, LocalDateTime startTime, LocalDateTime endTime, TransactionType type);
 }
