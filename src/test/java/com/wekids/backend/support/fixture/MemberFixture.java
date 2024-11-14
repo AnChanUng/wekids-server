3d9d4ec3c8093785cff5d9fc620f9e1d9982901c
yoon <cbj0010@naver.com>
2024-11-14 12:28:28 +0900

    [test/#8]: MemberFixture
    더미 값을 쉽게 넣을 수 있도록 test코드 작성 부분에 MeberFixture생성
    
    builder를 이용한 Member 인스턴스를 쉽게 생성 할 수 있도록 함

--- src/test/java/com/wekids/backend/support/fixture/MemberFixture.java
+++ src/test/java/com/wekids/backend/support/fixture/MemberFixture.java
@@ -1,5 +1,6 @@
 package com.wekids.backend.support.fixture;
 
+import com.wekids.backend.member.domain.Member;
 import com.wekids.backend.member.domain.enums.CardState;
 import com.wekids.backend.member.domain.enums.MemberState;
 
@@ -16,10 +17,10 @@ public class MemberFixture {
     private LocalDate birthday = LocalDate.of(1998, 5, 1); // 예시 생일
     private String profile = "https://image.com/virtual.png"; // 프로필 이미지 URL
     private MemberState state = MemberState.ACTIVE; // 상태
-    private LocalDate inactiveDate = null; // 비활성화 날짜는 null로 설정
+    private LocalDateTime inactiveDate = null; // 비활성화 날짜는 null로 설정
     private CardState cardState = CardState.NONE; // 카드 상태
     private Long bankMemberId = null; // 은행 회원 ID는 null로 설정
-    private LocalDateTime createdAt = LocalDateTime.now(); // 생성 날짜
+
 
     // 빌더 메서드
     public static MemberFixture builder() {
@@ -27,81 +28,76 @@ public class MemberFixture {
     }
 
     // 필요한 경우, 객체를 반환하는 메서드 추가
-    public MemberFixture withId(Long id) {
+    public MemberFixture id(Long id) {
         this.id = id;
         return this;
     }
 
-    public MemberFixture withName(String name) {
+    public MemberFixture name(String name) {
         this.name = name;
         return this;
     }
 
-    public MemberFixture withEmail(String email) {
+    public MemberFixture email(String email) {
         this.email = email;
         return this;
     }
 
-    public MemberFixture withSimplePassword(String simplePassword) {
+    public MemberFixture simplePassword(String simplePassword) {
         this.simplePassword = simplePassword;
         return this;
     }
 
-    public MemberFixture withPhone(String phone) {
+    public MemberFixture phone(String phone) {
         this.phone = phone;
         return this;
     }
 
-    public MemberFixture withBirthday(LocalDate birthday) {
+    public MemberFixture birthday(LocalDate birthday) {
         this.birthday = birthday;
         return this;
     }
 
-    public MemberFixture withProfile(String profile) {
+    public MemberFixture profile(String profile) {
         this.profile = profile;
         return this;
     }
 
-    public MemberFixture withState(MemberState state) {
+    public MemberFixture state(MemberState state) {
         this.state = state;
         return this;
     }
 
-    public MemberFixture withInactiveDate(LocalDate inactiveDate) {
+    public MemberFixture inactiveDate(LocalDateTime inactiveDate) {
         this.inactiveDate = inactiveDate;
         return this;
     }
 
-    public MemberFixture withCardState(CardState cardState) {
+    public MemberFixture cardState(CardState cardState) {
         this.cardState = cardState;
         return this;
     }
 
-    public MemberFixture withBankMemberId(Long bankMemberId) {
+    public MemberFixture bankMemberId(Long bankMemberId) {
         this.bankMemberId = bankMemberId;
         return this;
     }
 
-    public MemberFixture withCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-        return this;
-    }
 
     // Member 객체를 반환하는 메서드 (예시)
-//    public Member build() {
-//        Member member = new Member();
-//        member.setId(this.id);
-//        member.setName(this.name);
-//        member.setEmail(this.email);
-//        member.setSimplePassword(this.simplePassword);
-//        member.setPhone(this.phone);
-//        member.setBirthday(this.birthday);
-//        member.setProfile(this.profile);
-//        member.setState(this.state);
-//        member.setInactiveDate(this.inactiveDate);
-//        member.setCardState(this.cardState);
-//        member.setBankMemberId(this.bankMemberId);
-//        member.setCreatedAt(this.createdAt);
-//        return member;
-//    }
+    public Member build() {
+        return Member.builder()
+                .id(id)
+                .name(name)
+                .phone(phone)
+                .birthday(birthday)
+                .profile(profile)
+                .email(email)
+                .simplePassword(simplePassword)
+                .state(state)
+                .inactiveDate(inactiveDate)
+                .cardState(cardState)
+                .bankMemberId(bankMemberId)
+                .build();
+    }
 }
