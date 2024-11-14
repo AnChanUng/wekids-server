48dc0aaa145ec86bd911cff85d0119b2033900a3
jabin1221 <koreanjb1221@gmail.com>
2024-11-14 11:03:38 +0900

    [feat#/7]: 초기 세팅

--- src/main/java/com/wekids/backend/accountTransaction/controller/AccountTransactionController.java
+++ src/main/java/com/wekids/backend/accountTransaction/controller/AccountTransactionController.java
@@ -1,11 +1,25 @@
 package com.wekids.backend.accountTransaction.controller;
 
+import com.wekids.backend.accountTransaction.dto.response.TransactionListResponse;
+import com.wekids.backend.accountTransaction.service.AccountTransactionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class AccountTransactionController {
+
+    private final AccountTransactionService accountTransactionService;
+
+    @GetMapping("/api/v1/accounts/{accountId}/transactions")
+    public void getTransactionList(
+            @PathVariable("accountId") Long accountId,
+            @RequestParam(value = "start", required = false) String start,
+            @RequestParam(value = "end", required = false) String end,
+            @RequestParam(value = "type", required = false) String type,
+            @RequestParam(value = "page", required = false) int page,
+            @RequestParam(value = "size", required = false) int size) {
+    }
 }
