909b3c1f7d769553eb7112fc327cefb1b7fd01d1
안찬웅 <1stevering@naver.com>
2024-11-16 00:03:26 +0900

    [feat/#34]: 디자인 생성 controller 생성

---
+++ src/main/java/com/wekids/backend/design/controller/DesignController.java
@@ -0,0 +1,32 @@
+package com.wekids.backend.design.controller;
+
+import com.wekids.backend.design.dto.request.DesignCreateRequest;
+import com.wekids.backend.design.dto.response.DesignResponse;
+import com.wekids.backend.design.service.DesignService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/design")
+@RequiredArgsConstructor
+public class DesignController {
+
+    private final DesignService designService;
+
+    private static final Long DEFAULT_MEMBER_ID = 2L;
+
+    @GetMapping
+    public ResponseEntity<DesignResponse> getDesign() {
+        DesignResponse response = designService.showDesign(DEFAULT_MEMBER_ID);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping()
+    public ResponseEntity<DesignResponse> createDesign(@RequestBody DesignCreateRequest request) {
+        DesignResponse response = designService.createDesign(DEFAULT_MEMBER_ID, request);
+        return ResponseEntity.ok(response);
+    }
+}
