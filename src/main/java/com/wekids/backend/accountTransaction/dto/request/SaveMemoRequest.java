561531e7e708d439fa1fd059bd5ed9b0c9e71503
yoon <cbj0010@naver.com>
2024-11-15 15:54:40 +0900

    [feat/#10]: SaveMemoRequest
    메모저장 request 구현

--- src/main/java/com/wekids/backend/accountTransaction/dto/request/SaveMemoRequest.java
+++ src/main/java/com/wekids/backend/accountTransaction/dto/request/SaveMemoRequest.java
@@ -1,9 +1,9 @@
 package com.wekids.backend.accountTransaction.dto.request;
 
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
+import lombok.Getter;
 
-@Builder
+@Getter
 public class SaveMemoRequest {
 
     @Size(max = 20, message = "메모 내용은 최대 20자까지 입력 가능합니다.")
