32a892ae5a3e518d69a41215c1340d786ff86020
yoon <cbj0010@naver.com>
2024-11-18 01:13:58 +0900

    [feat/#9]: TransactionRequest 생성

--- src/main/java/com/wekids/backend/accountTransaction/dto/request/TransactionRequest.java
+++ src/main/java/com/wekids/backend/accountTransaction/dto/request/TransactionRequest.java
@@ -1,12 +1,28 @@
 package com.wekids.backend.accountTransaction.dto.request;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
 
+import java.math.BigDecimal;
+
+@Getter
 @Builder
 public class TransactionRequest {
+    @NonNull
     private String parentAccountNumber;
     private String childAccountNumber;
-    private Long amount;    //이체 금액
+    private BigDecimal amount;    //이체 금액
     private String sender;  //부모이름
     private String receiver;    //자식이름
+
+    private TransactionRequest() {}
+
+    public TransactionRequest(String parentAccountNumber, String childAccountNumber, BigDecimal amount, String sender, String receiver) {
+        this.parentAccountNumber = parentAccountNumber;
+        this.childAccountNumber = childAccountNumber;
+        this.amount = amount;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
 }
