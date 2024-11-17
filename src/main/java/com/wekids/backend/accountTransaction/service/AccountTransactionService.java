d7714dd2d1f26336009c8ac7108819575dcc5436
yoon <cbj0010@naver.com>
2024-11-18 01:15:02 +0900

    [feat/#9]: service
    service단 코드 추가

--- src/main/java/com/wekids/backend/accountTransaction/service/AccountTransactionService.java
+++ src/main/java/com/wekids/backend/accountTransaction/service/AccountTransactionService.java
@@ -1,7 +1,10 @@
 package com.wekids.backend.accountTransaction.service;
 
+import com.wekids.backend.accountTransaction.dto.request.TransactionRequest;
 import com.wekids.backend.accountTransaction.dto.response.TransactionDetailSearchResponse;
 
 public interface AccountTransactionService {
     TransactionDetailSearchResponse findByTransactionId(Long transactionId);
+
+    void saveTransaction(TransactionRequest transactionRequest);
 }
--- src/main/java/com/wekids/backend/accountTransaction/service/AccountTransactionServiceImpl.java
+++ src/main/java/com/wekids/backend/accountTransaction/service/AccountTransactionServiceImpl.java
@@ -1,6 +1,10 @@
 package com.wekids.backend.accountTransaction.service;
 
+import com.wekids.backend.account.domain.Account;
+import com.wekids.backend.account.repository.AccountRepository;
 import com.wekids.backend.accountTransaction.domain.AccountTransaction;
+import com.wekids.backend.accountTransaction.domain.enums.TransactionType;
+import com.wekids.backend.accountTransaction.dto.request.TransactionRequest;
 import com.wekids.backend.accountTransaction.dto.response.TransactionDetailSearchResponse;
 import com.wekids.backend.accountTransaction.repository.AccountTransactionRepository;
 import com.wekids.backend.exception.ErrorCode;
@@ -10,6 +14,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -17,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class AccountTransactionServiceImpl implements AccountTransactionService {
 
     private final AccountTransactionRepository accountTransactionRepository;
+    private final AccountRepository accountRepository;
 
 
     @Override
@@ -25,6 +33,48 @@ public class AccountTransactionServiceImpl implements AccountTransactionService 
         AccountTransaction accountTransaction = accountTransactionById(transactionId);
         return TransactionDetailSearchResponse.from(accountTransaction);
 
+    }
+
+    @Override
+    @Transactional
+    public void saveTransaction(TransactionRequest transactionRequest) {
+        Account parentAccount = getAccountById(transactionRequest.getParentAccountNumber());
+        Account childAccount = getAccountById(transactionRequest.getChildAccountNumber());
+
+        AccountTransaction parentTransaction = createParentAccountTransaction(transactionRequest, parentAccount);
+        AccountTransaction childTransaction = createChildAccountTransaction(transactionRequest, childAccount);
+
+        accountTransactionRepository.save(parentTransaction);
+        accountTransactionRepository.save(childTransaction);
+    }
+
+    private Account getAccountById(String accountNumber) {
+        return accountRepository.findAccountByAccountNumber(accountNumber)
+                .orElseThrow(() -> new WekidsException(ErrorCode.MEMBER_NOT_FOUND,
+                        "회원 id: " + accountNumber));
+    }
+
+    private AccountTransaction createParentAccountTransaction(TransactionRequest request, Account parentAccount) {
+        BigDecimal newBalance = parentAccount.getBalance().subtract(request.getAmount());
+        return createTransaction(request, TransactionType.WITHDRAWAL, newBalance);
+    }
+
+    private AccountTransaction createChildAccountTransaction(TransactionRequest request, Account childAccount) {
+        BigDecimal newBalance = childAccount.getBalance().add(request.getAmount());
+        return createTransaction(request, TransactionType.DEPOSIT, newBalance);
+    }
+
+    private AccountTransaction createTransaction(TransactionRequest request, TransactionType type, BigDecimal balance) {
+        return AccountTransaction.builder()
+                .title(request.getSender())
+                .type(type)
+                .amount(request.getAmount())
+                .balance(balance)
+                .sender(request.getSender())
+                .receiver(request.getReceiver())
+                .memo("") // Optional: 나중에 기본값이나 다른 설정 고려 가능
+                .createdAt(LocalDateTime.now())
+                .build();
     }
 
     private AccountTransaction accountTransactionById(Long transactionId) {
