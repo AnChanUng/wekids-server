3c96d267e10de0ab11443ca1cc9c58275fcc3fed
안찬웅 <1stevering@naver.com>
2024-11-18 19:23:39 +0900

    [file/#16]: 불필요한 코드 삭제

--- src/main/java/com/wekids/backend/member/repository/ChildRepository.java
+++ src/main/java/com/wekids/backend/member/repository/ChildRepository.java
@@ -2,11 +2,7 @@ package com.wekids.backend.member.repository;
 
 import com.wekids.backend.member.domain.Child;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface ChildRepository extends JpaRepository<Child, Long> {
