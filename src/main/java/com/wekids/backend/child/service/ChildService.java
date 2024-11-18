23ee75e7ba6797c9fced15181552553e92f35b2c
안찬웅 <1stevering@naver.com>
2024-11-18 18:18:11 +0900

    [file/#16]: 불필요한 파일 삭제

--- src/main/java/com/wekids/backend/child/service/ChildService.java
+++
@@ -1,7 +0,0 @@
-package com.wekids.backend.child.service;
-
-import com.wekids.backend.child.dto.response.ChildAccountResponse;
-
-public interface ChildService {
-    public ChildAccountResponse getChildAccount();
-}
