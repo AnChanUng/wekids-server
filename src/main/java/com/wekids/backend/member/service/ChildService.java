44cf38e7eb7aa308e2b937dcefe4c32bc12cbbeb
안찬웅 <1stevering@naver.com>
2024-11-18 17:17:56 +0900

    [feat/#16]: ChildService 생성

---
+++ src/main/java/com/wekids/backend/member/service/ChildService.java
@@ -0,0 +1,7 @@
+package com.wekids.backend.member.service;
+
+import com.wekids.backend.member.dto.response.ChildResponse;
+
+public interface ChildService {
+    ChildResponse getChildAccount();
+}
