f11a36e403841dbb6a58b04cc3bae06908d4735b
yoon <cbj0010@naver.com>
2024-11-19 01:37:01 +0900

    [refact/#10]: 수정
    builder 어노테이션으로 변경

--- src/main/java/com/wekids/backend/accountTransaction/dto/request/UpdateMemoRequest.java
+++ src/main/java/com/wekids/backend/accountTransaction/dto/request/UpdateMemoRequest.java
@@ -1,15 +1,18 @@
 package com.wekids.backend.accountTransaction.dto.request;
 
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UpdateMemoRequest {
 
     @Size(max = 20, message = "메모 내용은 최대 20자까지 입력 가능합니다.")
     private String memo;
 
-    private UpdateMemoRequest(){}
-
-    public UpdateMemoRequest(String memo){this.memo = memo;}
 }
