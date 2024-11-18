4bc047a54197d5fd2f3ba5dd083b55dd7311057b
안찬웅 <1stevering@naver.com>
2024-11-18 13:30:42 +0900

    [refact/#34]: DesignCreateRequest 필요 없는 어노테이션 삭제

--- src/main/java/com/wekids/backend/design/dto/request/DesignCreateRequest.java
+++ src/main/java/com/wekids/backend/design/dto/request/DesignCreateRequest.java
@@ -1,12 +1,10 @@
 package com.wekids.backend.design.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
 @Getter
-@Setter
-@ToString
+@Builder
 public class DesignCreateRequest {
     private String color;
     private String character;
