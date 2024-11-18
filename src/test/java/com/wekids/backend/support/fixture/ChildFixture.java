f2a8e89c0ad8af653976c3b7434f6ec9843e7b23
안찬웅 <1stevering@naver.com>
2024-11-18 18:15:59 +0900

    [refact/#16]: ChildFixture @Bulder 추가

--- src/test/java/com/wekids/backend/support/fixture/ChildFixture.java
+++ src/test/java/com/wekids/backend/support/fixture/ChildFixture.java
@@ -3,11 +3,12 @@ package com.wekids.backend.support.fixture;
 import com.wekids.backend.member.domain.Child;
 import com.wekids.backend.member.domain.enums.CardState;
 import com.wekids.backend.member.domain.enums.MemberState;
+import lombok.Builder;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-
+@Builder
 public class ChildFixture {
 
     private Long id;
@@ -21,65 +22,6 @@ public class ChildFixture {
     private LocalDateTime inactiveDate = null; // 비활성화 날짜는 null로 설정
     private CardState cardState = CardState.NONE; // 카드 상태
     private Long bankMemberId = null; // 은행 회원 ID는 null로 설정
-
-    public static ChildFixture builder() {
-        return new ChildFixture();
-    }
-
-    public ChildFixture id(Long id) {
-        this.id = id;
-        return this;
-    }
-
-    public ChildFixture name(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public ChildFixture email(String email) {
-        this.email = email;
-        return this;
-    }
-
-    public ChildFixture simplePassword(String simplePassword) {
-        this.simplePassword = simplePassword;
-        return this;
-    }
-
-    public ChildFixture phone(String phone) {
-        this.phone = phone;
-        return this;
-    }
-
-    public ChildFixture birthday(LocalDate birthday) {
-        this.birthday = birthday;
-        return this;
-    }
-
-    public ChildFixture profile(String profile) {
-        this.profile = profile;
-        return this;
-    }
-
-    public ChildFixture state(MemberState state) {
-        this.state = state;
-        return this;
-    }
-
-    public ChildFixture inactiveDate(LocalDateTime inactiveDate) {
-        this.inactiveDate = inactiveDate;
-        return this;
-    }
-
-    public ChildFixture cardState(CardState cardState) {
-        this.cardState = cardState;
-        return this;
-    }
-
-    public ChildFixture bankMemberId(Long bankMemberId) {
-        this.bankMemberId = bankMemberId;
-        return this;
-    }
 
     public Child build() {
         return Child.builder()
