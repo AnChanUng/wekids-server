018784d66b299a6c30b2ccdd01a31ee60aa95ffa
yoon <cbj0010@naver.com>
2024-11-18 15:06:08 +0900

    [test/#9]: lombok추가
    AccountFixture builder 어노테이션으로 변환

--- src/test/java/com/wekids/backend/support/fixture/AccountFixture.java
+++ src/test/java/com/wekids/backend/support/fixture/AccountFixture.java
@@ -3,54 +3,35 @@ package com.wekids.backend.support.fixture;
 import com.wekids.backend.account.domain.Account;
 import com.wekids.backend.account.domain.enums.AccountState;
 import com.wekids.backend.member.domain.Member;
+import lombok.Builder;
+import lombok.Getter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Getter
+@Builder
 public class AccountFixture {
+
+    @Builder.Default
     private Long id = 1L;
-    private String accountNumber = "강현우"; // 더미 데이터 계좌 번호
+
+    @Builder.Default
+    private String accountNumber = "account1234567"; // 더미 데이터 계좌 번호
+
+    @Builder.Default
     private BigDecimal balance = BigDecimal.valueOf(100.00); // 초기 잔액
+
+    @Builder.Default
     private AccountState state = AccountState.ACTIVE; // 계좌 상태
+
+    @Builder.Default
     private LocalDateTime createAt = LocalDateTime.now(); // 생성 시간
+
     private LocalDateTime inactiveDate; // 비활성화 날짜
     private Member member; // 회원 정보
 
-    public static AccountFixture builder() {
-        return new AccountFixture();
-    }
-
-    public AccountFixture id(Long id) {
-        this.id = id;
-        return this;
-    }
-
-    public AccountFixture accountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-        return this;
-    }
-
-    public AccountFixture balance(BigDecimal balance) {
-        this.balance = balance;
-        return this;
-    }
-
-    public AccountFixture state(AccountState state) {
-        this.state = state;
-        return this;
-    }
-
-    public AccountFixture inactiveDate(LocalDateTime inactiveDate) {
-        this.inactiveDate = inactiveDate;
-        return this;
-    }
-
-    public AccountFixture member(Member member) {
-        this.member = member;
-        return this;
-    }
-
-    public Account build() {
+    public Account toAccount() {
         return Account.builder()
                 .id(id)
                 .accountNumber(accountNumber)
