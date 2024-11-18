f16385749101f91af642794f45eeae57d318bd2a
안찬웅 <1stevering@naver.com>
2024-11-18 17:14:12 +0900

    [feat/#16]: Child @ToString 추가

--- src/main/java/com/wekids/backend/member/domain/Child.java
+++ src/main/java/com/wekids/backend/member/domain/Child.java
@@ -4,11 +4,13 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 @Entity
 @DiscriminatorValue("CHILD")
 @Getter
+@ToString
 @SuperBuilder
 @NoArgsConstructor
 public class Child extends Member {
