f8aee74ae6be35d27f5dc14afbae6dd1b4365ed9
jabin1221 <koreanjb1221@gmail.com>
2024-11-18 12:43:48 +0900

    [feat/#43]: 예외 코드 수정

--- src/main/java/com/wekids/backend/account/service/AccountServiceImpl.java
+++ src/main/java/com/wekids/backend/account/service/AccountServiceImpl.java
@@ -33,12 +33,12 @@ public class AccountServiceImpl implements AccountService{
     @Override
     public List<AccountChildListResponse> findAccountChildList(long parentId){
         List<Child> children = memberRepository.findChildrenByParentId(parentId)
-                .orElseThrow(() -> new WekidsException(ErrorCode.MEMBER_NOT_FOUND, "자식들 정보를 찾을 수 없습니다."));// parent_child 테이블을 통해 자녀 목록 조회
+                .orElseThrow(() -> new WekidsException(ErrorCode.MEMBER_NOT_FOUND, parentId + "의 자식들 정보를 찾을 수 없습니다."));// parent_child 테이블을 통해 자녀 목록 조회
         // 각 자녀의 계좌 정보 조회
         return children.stream().map(child -> {
             // 각 자녀의 계좌 정보 조회
             Account childAccount = accountRepository.findByMember(child)
-                    .orElseThrow(() -> new WekidsException(ErrorCode.ACCOUNT_NOT_FOUND, "자식 계좌를 찾을 수 없습니다."));
+                    .orElseThrow(() -> new WekidsException(ErrorCode.ACCOUNT_NOT_FOUND, "자식" + child.getId() + "의 계좌를 찾을 수 없습니다."));
             return AccountChildListResponse.from(childAccount);
         }).toList();
     }
