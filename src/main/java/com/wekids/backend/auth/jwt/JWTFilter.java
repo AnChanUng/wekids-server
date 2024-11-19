e1a9dadf555c1918304ccb01b8adecbf0f980401
Kang Hyun Woo <gusdn741021@gmail.com>
2024-11-20 00:00:00 +0900

    [feat/#47]: jwt 기능 작성
     - jwt util
     - jwt filter

---
+++ src/main/java/com/wekids/backend/auth/jwt/JWTFilter.java
@@ -0,0 +1,52 @@
+package com.wekids.backend.auth.jwt;
+
+import com.wekids.backend.auth.dto.CustomOAuth2User;
+import com.wekids.backend.auth.enums.LoginState;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+@RequiredArgsConstructor
+public class JWTFilter extends OncePerRequestFilter {
+    private final JWTUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = null;
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie : cookies){
+            if(cookie.getName().equals("Authorization")){
+                authorization = cookie.getValue();
+            }
+        }
+
+        if(authorization == null){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authorization;
+        if(jwtUtil.isExpired(token)){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        Long memberId = jwtUtil.getMemberId(token);
+        String role = jwtUtil.getRole(token);
+        CustomOAuth2User customOAuth2User = CustomOAuth2User.of(LoginState.LOGIN, role, memberId);
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customOAuth2User, null, customOAuth2User.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
+    }
+}
