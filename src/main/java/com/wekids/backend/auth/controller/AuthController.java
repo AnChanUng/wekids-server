6bb609cdcd32d5307873b7b7d2f20905eab16243
Kang Hyun Woo <gusdn741021@gmail.com>
2024-11-20 12:12:32 +0900

    [feat/#47]: 회원가입 controller 작성

---
+++ src/main/java/com/wekids/backend/auth/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.wekids.backend.auth.controller;
+
+import com.wekids.backend.auth.dto.SignUpRequest;
+import com.wekids.backend.auth.dto.SignUpResponse;
+import com.wekids.backend.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/api/v1/signup")
+    public ResponseEntity<SignUpResponse> join(@RequestBody SignUpRequest signUpRequest){
+        SignUpResponse response = authService.signup(signUpRequest);
+        return new ResponseEntity<>(response, CREATED);
+    }
+}
