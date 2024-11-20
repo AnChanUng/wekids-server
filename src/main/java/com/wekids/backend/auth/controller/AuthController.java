f25982378248b976d4f19690cedb4ca4e651cfa6
Kang Hyun Woo <gusdn741021@gmail.com>
2024-11-20 18:56:18 +0900

    [refact/#47]: @Valid 추가

--- src/main/java/com/wekids/backend/auth/controller/AuthController.java
+++ src/main/java/com/wekids/backend/auth/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.wekids.backend.auth.controller;
 import com.wekids.backend.auth.dto.request.SignUpRequest;
 import com.wekids.backend.auth.dto.response.SignUpResponse;
 import com.wekids.backend.auth.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/api/v1/signup")
-    public ResponseEntity<SignUpResponse> join(@RequestBody SignUpRequest signUpRequest){
+    public ResponseEntity<SignUpResponse> join(@Valid @RequestBody SignUpRequest signUpRequest){
         SignUpResponse response = authService.signup(signUpRequest);
         return new ResponseEntity<>(response, CREATED);
     }
