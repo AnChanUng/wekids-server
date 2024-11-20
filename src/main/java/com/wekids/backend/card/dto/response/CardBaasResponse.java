8d863337aa89c01457eb107bd9e91efc89592042
안찬웅 <1stevering@naver.com>
2024-11-20 19:02:34 +0900

    [feat/#41]: CardBaasResponse 생성

---
+++ src/main/java/com/wekids/backend/card/dto/response/CardBaasResponse.java
@@ -0,0 +1,30 @@
+package com.wekids.backend.card.dto.response;
+
+import com.wekids.backend.card.domain.Card;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@ToString
+public class CardBaasResponse {
+    private String cardNumber;
+    private LocalDate validThru;
+    private String cvc;
+    private String bankMemberName;
+    private LocalDateTime newDate;
+
+    public static Card of(String cardNumber, LocalDate validThru, String cvc, String memberName, LocalDateTime newDate) {
+        return Card.builder()
+                .cardNumber(cardNumber)
+                .validThru(validThru)
+                .cvc(cvc)
+                .memberName(memberName)
+                .newDate(newDate)
+                .build();
+    }
+}
