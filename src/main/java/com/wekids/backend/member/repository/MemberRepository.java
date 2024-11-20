b8d946c36f5c87cce2129763a19185ce4e6a362f
안찬웅 <1stevering@naver.com>
2024-11-20 21:40:27 +0900

    [feat/#41]: childId로 Parent 엔티티 가져옴

--- src/main/java/com/wekids/backend/member/repository/MemberRepository.java
+++ src/main/java/com/wekids/backend/member/repository/MemberRepository.java
@@ -1,9 +1,18 @@
 package com.wekids.backend.member.repository;
 
 import com.wekids.backend.member.domain.Member;
+import com.wekids.backend.member.domain.Parent;
+import com.wekids.backend.member.domain.mapping.ParentChild;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByBankMemberId(Long bankMemberId);
+
+    @Query("select p from Child c join ParentChild pc on c = pc.child join Parent p on pc.parent = p where c.id = :childId")
+    Optional<Parent> findParentByChildId(Long childId);
 }
