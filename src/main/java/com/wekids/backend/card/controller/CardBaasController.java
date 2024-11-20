230e10bb21f8692a4ca82591fd32cbc65ac9ec5b
안찬웅 <1stevering@naver.com>
2024-11-20 18:58:04 +0900

    [feat/#41]: Controller 생성
    나는 깃메시지 이렇게 만들자는거였는데ㅋㅋㅋㅋㅋㅋㅋㅋ
    [feat/#1]: 초기세팅
    
    어쩌구저쩌구 수정했습니다.

---
+++ src/main/java/com/wekids/backend/card/controller/CardBaasController.java
@@ -0,0 +1,28 @@
+package com.wekids.backend.card.controller;
+
+import com.wekids.backend.card.dto.request.PasswordRequest;
+import com.wekids.backend.card.service.CardBaasService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/accounts/cards")
+@RequiredArgsConstructor
+@Log4j2
+public class CardBaasController {
+
+    private final CardBaasService cardBaasService;
+
+    @PostMapping("/issue")
+    public ResponseEntity<Void> issueCard(@Valid @RequestBody PasswordRequest passwordRequest) {
+        cardBaasService.issueAccountAndCard(passwordRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
