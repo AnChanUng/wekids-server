3d1fb8bb21cc62db011cd844b8e3bfc4d1c21144
Kang Hyun Woo <gusdn741021@gmail.com>
2024-11-21 10:31:01 +0900

    [test/#47]: PasswordEncoder 추가
    AuthServiceImplTest에 PasswordEncoder 추가

--- src/test/java/com/wekids/backend/auth/service/AuthServiceImplTest.java
+++ src/test/java/com/wekids/backend/auth/service/AuthServiceImplTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
 
@@ -23,6 +24,7 @@ import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 @ExtendWith(MockitoExtension.class)
 class AuthServiceImplTest {
+    @Mock PasswordEncoder passwordEncoder;
     @Mock JWTUtil jwtUtil;
     @Mock private MemberRepository memberRepository;
     @InjectMocks private AuthServiceImpl authService;
