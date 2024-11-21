f26b9d23bb7095f441b1c9a733b121ba182bfd8a
안찬웅 <1stevering@naver.com>
2024-11-21 09:10:35 +0900

    [refact/#41]: PasswordRquest builder notblank 어노테이션 추가

--- src/main/java/com/wekids/backend/card/dto/request/PasswordRequest.java
+++ src/main/java/com/wekids/backend/card/dto/request/PasswordRequest.java
@@ -1,11 +1,13 @@
 package com.wekids.backend.card.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class PasswordRequest {
     @NotBlank
     private String password;
