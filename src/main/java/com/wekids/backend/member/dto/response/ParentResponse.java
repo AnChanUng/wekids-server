865a80d199374dab338a0f65346f34b541b9c16f
Kang Hyun Woo <gusdn741021@gmail.com>
2024-11-21 16:18:16 +0900

    [refact/#15]: 오류 수정 및 없는 객체 안 보이도록 수정
     - 계좌의 id가 부모의 id로 반환하도록 되어 있음
     - 없는 객체들은 제외해서 처리하도록 변경

--- src/main/java/com/wekids/backend/member/dto/response/ParentResponse.java
+++ src/main/java/com/wekids/backend/member/dto/response/ParentResponse.java
@@ -1,10 +1,13 @@
 package com.wekids.backend.member.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.wekids.backend.account.domain.Account;
 import com.wekids.backend.design.domain.Design;
+import com.wekids.backend.design.domain.enums.CharacterType;
 import com.wekids.backend.design.domain.enums.ColorType;
 import com.wekids.backend.member.domain.Parent;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,15 +16,34 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ParentResponse {
     String name;
     String accountNumber;
     String profile;
     BigDecimal balance;
-    ColorType designType;
-    long accountId;
+    ColorType color;
+    CharacterType character;
+    Long accountId;
 
-    public static ParentResponse of(Parent parent, Account account, Design design){
-        return new ParentResponse(parent.getName(), account.getAccountNumber(), parent.getProfile(), account.getBalance(), design.getColor(), account.getId());
+    public static ParentResponse of(Parent parent, Account account, Design design) {
+        ParentResponse.ParentResponseBuilder builder = ParentResponse.builder()
+                .name(parent.getName())
+                .profile(parent.getProfile());
+
+        if (account != null) {
+            builder.accountId(account.getId())
+                    .accountNumber(account.getAccountNumber())
+                    .balance(account.getBalance());
+        }
+
+        if (design != null) {
+            builder.color(design.getColor());
+            builder.character(design.getCharacter());
+        }
+
+        return builder.build();
     }
+
 }
