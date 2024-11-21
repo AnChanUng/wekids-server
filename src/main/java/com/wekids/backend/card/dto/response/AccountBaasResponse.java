919fdc761381cfcd18c8c05bd336a11cbb6ec453
안찬웅 <1stevering@naver.com>
2024-11-21 12:52:56 +0900

    [refact#41]: 생성자 추가

--- src/main/java/com/wekids/backend/card/dto/response/AccountBaasResponse.java
+++ src/main/java/com/wekids/backend/card/dto/response/AccountBaasResponse.java
@@ -1,12 +1,16 @@
 package com.wekids.backend.card.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountBaasResponse {
     private String accountNumber; // 계좌번호
     private String type;          // 계좌 종류
