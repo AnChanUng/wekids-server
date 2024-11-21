f4e5c83705982162b4b55a10d3065dd6ef2afd74
yoon <cbj0010@naver.com>
2024-11-21 12:38:42 +0900

    [refact/#9]: bean validation추가

--- src/main/java/com/wekids/backend/accountTransaction/dto/request/TransactionRequest.java
+++ src/main/java/com/wekids/backend/accountTransaction/dto/request/TransactionRequest.java
@@ -1,5 +1,8 @@
 package com.wekids.backend.accountTransaction.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -9,10 +12,16 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TransactionRequest {
+    @NotBlank(message = "보내는 분 계좌번호는 빈값일 수 없습니다")
     private String parentAccountNumber;
+    @NotBlank(message = "받는 분 계좌번호는 빈값일 수 없습니다")
     private String childAccountNumber;
+    @NotNull
+    @Positive(message = "이체 금액은 0보다 커야 합니다")
     private BigDecimal amount;    //이체 금액
+    @NotBlank(message = "보내는 분은 빈값일 수 없습니다")
     private String sender;  //부모이름
+    @NotBlank(message = "받는 분은 빈값일 수 없습니다")
     private String receiver;    //자식이름
 
 }
