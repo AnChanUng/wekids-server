9366c5daa4ad5e574f577326f01813d198c61680
안찬웅 <1stevering@naver.com>
2024-11-21 12:52:39 +0900

    [refact/#41]: 정적 팩토리 메소드로 변경

--- src/main/java/com/wekids/backend/card/dto/request/AccountBaasRequest.java
+++ src/main/java/com/wekids/backend/card/dto/request/AccountBaasRequest.java
@@ -1,5 +1,7 @@
 package com.wekids.backend.card.dto.request;
 
+import com.wekids.backend.member.domain.Member;
+import com.wekids.backend.member.domain.Parent;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,4 +11,13 @@ public class AccountBaasRequest {
     private Long bankMemberId;
     private Long productId;
     private String password;
+
+
+    public static AccountBaasRequest createFromMemberAndParent(Member member, Parent parent, Long productId) {
+        return AccountBaasRequest.builder()
+                .bankMemberId(member.getBankMemberId())
+                .productId(productId)
+                .password(parent.getSimplePassword())
+                .build();
+    }
 }
