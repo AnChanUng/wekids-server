8b79d725501736573f8bf26e97972ea31a8598d3
YE-EUN <musiclove529@naver.com>
2024-11-22 16:36:43 +0900

    [feat/#41]: 서비스 코드 수정
    은행 고객 생성 로직 추가
    RestTemplate 응답 ResponseEntity로 받도록 수정

--- src/main/java/com/wekids/backend/card/service/CardBaasService.java
+++
@@ -1,8 +0,0 @@
-package com.wekids.backend.card.service;
-
-import com.wekids.backend.card.dto.request.PasswordRequest;
-
-public interface CardBaasService {
-    void issueAccountAndCard(PasswordRequest passwordRequest);
-
-}
