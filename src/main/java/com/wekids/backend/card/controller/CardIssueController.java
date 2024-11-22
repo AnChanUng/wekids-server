be016d5377cf6b444b3716bb36fdedd20084ca6b
YE-EUN <musiclove529@naver.com>
2024-11-22 16:37:21 +0900

    [feat/#41]: 계좌, 카드 생성 컨트롤러 수정

--- src/main/java/com/wekids/backend/card/controller/CardIssueController.java
+++ src/main/java/com/wekids/backend/card/controller/CardIssueController.java
@@ -1,7 +1,7 @@
 package com.wekids.backend.card.controller;
 
-import com.wekids.backend.card.dto.request.PasswordRequest;
-import com.wekids.backend.card.service.CardBaasService;
+import com.wekids.backend.card.dto.request.IssueRequest;
+import com.wekids.backend.card.service.CardIssueService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,15 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/accounts/cards")
+@RequestMapping("/api/v1/accounts/cards/issue")
 @RequiredArgsConstructor
-public class CardBaasController {
+public class CardIssueController {
 
-    private final CardBaasService cardBaasService;
+    private final CardIssueService cardBaasService;
 
-    @PostMapping("/issue")
-    public ResponseEntity<Void> issueCard(@Valid @RequestBody PasswordRequest passwordRequest) {
-        cardBaasService.issueAccountAndCard(passwordRequest);
+    @PostMapping
+    public ResponseEntity<Void> issueCard(@Valid @RequestBody IssueRequest issueRequest) {
+        cardBaasService.issueAccountAndCard(issueRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
