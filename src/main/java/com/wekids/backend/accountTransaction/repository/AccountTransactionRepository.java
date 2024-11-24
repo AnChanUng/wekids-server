9f0ed2e775132a2cb6a56bf12ceea71888c5525a
Kang Hyun Woo <gusdn741021@gmail.com>
2024-11-25 00:07:46 +0900

    [feat/#7]: 동적쿼리로 거래내역 조회 구현
     -qureydsl를 사용하서 타입에 따라 거내래역 조회

--- src/main/java/com/wekids/backend/accountTransaction/repository/AccountTransactionRepository.java
+++ src/main/java/com/wekids/backend/accountTransaction/repository/AccountTransactionRepository.java
@@ -1,20 +1,9 @@
 package com.wekids.backend.accountTransaction.repository;
 
 import com.wekids.backend.accountTransaction.domain.AccountTransaction;
-import com.wekids.backend.accountTransaction.domain.enums.TransactionType;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.List;
 
-@Repository
-public interface AccountTransactionRepository extends JpaRepository<AccountTransaction,Long> {
-
-    Slice<AccountTransaction> findSliceBySenderOrReceiverAndCreatedAtBetweenAndType(
-            Pageable limit, String sender, String receiver, LocalDateTime startTime, LocalDateTime endTime, TransactionType type);
-    Slice<AccountTransaction> findSliceBySenderOrReceiverAndCreatedAtBetween(
-            Pageable limit, String sender, String receiver, LocalDateTime startTime, LocalDateTime endTime);
+public interface AccountTransactionRepository
+        extends JpaRepository<AccountTransaction,Long>, AccountTransactionCustomRepository {
 }
