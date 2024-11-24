4610ec23d68c5809782d91e6400a4d8b98b562c7
Kang Hyun Woo <gusdn741021@gmail.com>
2024-11-25 00:04:46 +0900

    [feat/#7]: 에러 코드 추가

--- src/main/java/com/wekids/backend/exception/ErrorCode.java
+++ src/main/java/com/wekids/backend/exception/ErrorCode.java
@@ -3,16 +3,21 @@ package com.wekids.backend.exception;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Getter
 public enum ErrorCode {
     INVALID_INPUT(BAD_REQUEST, "잘못된 입력 값입니다."),
     MEMBER_NOT_FOUND(NOT_FOUND, "회원을 찾을 수 없습니다."),
     TRANSACTION_NOT_FOUND(NOT_FOUND, "거래 내역을 찾을 수 없습니다."),
-    ACCOUNT_NOT_FOUND(NOT_FOUND, "회원을 찾을 수 없습니다."),
-    INVALID_DATE_FORMAT(BAD_REQUEST, "잘못된 입력 값입니다."),
-    INVALID_TRANSACTION(BAD_REQUEST, "잘못된 입력 값입니다.");
+    INVALID_TRANSACTION_AMOUNT(BAD_REQUEST, "거래 할 수 없는 금액입니다."),
+    INVALID_ACCOUNT_NUMBER(NOT_FOUND, "계좌번호를 찾을 수 없습니다."),
+    ACCOUNT_NOT_ACTIVE(NOT_FOUND, "정지된 계좌입니다."),
+    ACCOUNT_NOT_FOUND(NOT_FOUND, "계좌를 찾을 수 없습니다."),
+    BAAS_NOT_RESPONSE(HttpStatus.GATEWAY_TIMEOUT, "BaaS 응답이 없습니다."),
+    BAAS_REQUEST_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "BaaS 요청에 실패했습니다."),
+    ;
 
     private final HttpStatus status;
     private final String message;
