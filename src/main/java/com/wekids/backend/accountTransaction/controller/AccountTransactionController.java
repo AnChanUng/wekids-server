eee072a03034ade37f837f0a3192c2ca8c2d5d3d
Kang Hyun Woo <gusdn741021@gmail.com>
2024-11-25 00:17:26 +0900

    [refact/#7]: 요청에 맞게 거래내역 조회 get 부분 수정

--- src/main/java/com/wekids/backend/accountTransaction/controller/AccountTransactionController.java
+++ src/main/java/com/wekids/backend/accountTransaction/controller/AccountTransactionController.java
@@ -1,14 +1,15 @@
 package com.wekids.backend.accountTransaction.controller;
 
+import com.wekids.backend.accountTransaction.dto.enums.TransactionRequestType;
 import com.wekids.backend.accountTransaction.dto.request.UpdateMemoRequest;
 import com.wekids.backend.accountTransaction.dto.response.TransactionDetailSearchResponse;
-import com.wekids.backend.accountTransaction.dto.response.TransactionGetResponse;
-import com.wekids.backend.accountTransaction.service.AccountTransactionService;
-import com.wekids.backend.accountTransaction.dto.response.TransactionListResponse;
+import com.wekids.backend.accountTransaction.dto.response.TransactionHistoryResponse;
 import com.wekids.backend.accountTransaction.service.AccountTransactionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,10 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import java.time.LocalDate;
+
 
 @RestController
 @RequestMapping("api/v1/transactions")
@@ -41,15 +40,14 @@ public class AccountTransactionController {
         return ResponseEntity.ok(result);
     }
 
-    @GetMapping("/{accountId}/transactions")
-    public ResponseEntity<List<TransactionGetResponse>> getTransactionList(
+    @GetMapping("/accounts/{accountId}")
+    public ResponseEntity<TransactionHistoryResponse> getTransactionList(
             @PathVariable("accountId") Long accountId,
-            @RequestParam(value = "start", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime start,
-            @RequestParam(value = "end", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime end,
-            @RequestParam(value = "type", required = false, defaultValue = "ALL") String type,
-            @RequestParam(value = "page", required = false, defaultValue = "0") int page,
-            @RequestParam(value = "size", required = false, defaultValue = "5") int size) {
-        Slice<TransactionGetResponse> responseList = accountTransactionService.showTransactionList(accountId, start, end, type, page, size);
-            return ResponseEntity.ok(responseList.getContent());
+            @RequestParam(value = "start") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate start,
+            @RequestParam(value = "end") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end,
+            @RequestParam(defaultValue = "ALL") TransactionRequestType type,
+            @PageableDefault(size = 20) Pageable pageable) {
+        TransactionHistoryResponse transactionHistoryResponse = accountTransactionService.showTransactionList(accountId, start, end, type, pageable);
+        return ResponseEntity.ok(transactionHistoryResponse);
     }
 }
