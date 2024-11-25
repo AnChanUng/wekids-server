1104d86c9f4c503391786dec95e92398e8e63cea
Kang Hyun Woo <gusdn741021@gmail.com>
2024-11-25 11:58:02 +0900

    [refact/#7]: 테스트 코드 수정

--- src/test/java/com/wekids/backend/accountTransaction/service/AccountTransactionServiceImplTest.java
+++ src/test/java/com/wekids/backend/accountTransaction/service/AccountTransactionServiceImplTest.java
@@ -3,41 +3,27 @@ package com.wekids.backend.accountTransaction.service;
 import com.wekids.backend.account.domain.Account;
 import com.wekids.backend.account.repository.AccountRepository;
 import com.wekids.backend.accountTransaction.domain.AccountTransaction;
-import com.wekids.backend.accountTransaction.dto.response.TransactionListResponse;
 import com.wekids.backend.account.domain.enums.AccountState;
-import com.wekids.backend.accountTransaction.domain.AccountTransaction;
 import com.wekids.backend.accountTransaction.domain.enums.TransactionType;
 import com.wekids.backend.accountTransaction.dto.request.UpdateMemoRequest;
 import com.wekids.backend.accountTransaction.dto.response.TransactionDetailSearchResponse;
 import com.wekids.backend.accountTransaction.repository.AccountTransactionRepository;
 import com.wekids.backend.support.fixture.AccountFixture;
 import com.wekids.backend.support.fixture.AccountTransactionFixture;
-import com.wekids.backend.support.fixture.ChildFixture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
