dfa2d13f28ccc164d43cc66adbbd68db9e71e041
YE-EUN <musiclove529@naver.com>
2024-11-26 12:46:01 +0900

    [feat/#50]: 부모 계좌 조회 동의 컨트롤러 추가

--- src/main/java/com/wekids/backend/account/controller/AccountController.java
+++ src/main/java/com/wekids/backend/account/controller/AccountController.java
@@ -5,9 +5,7 @@ import com.wekids.backend.account.dto.response.AccountResponse;
 import com.wekids.backend.account.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
