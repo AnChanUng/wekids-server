69dd13020a4f9bc3b71803d42f7cbca6fea34ce4
YE-EUN <musiclove529@naver.com>
2024-12-02 16:00:40 +0900

    [feat/#59]: 미션 조회 요청 응답 dto 생성

--- src/main/java/com/wekids/backend/mission/dto/request/MissionListGetRequestParams.java
+++ src/main/java/com/wekids/backend/mission/dto/request/MissionListGetRequestParams.java
@@ -1,4 +1,18 @@
 package com.wekids.backend.mission.dto.request;
 
+import com.wekids.backend.mission.domain.enums.MissionCategory;
+import com.wekids.backend.mission.domain.enums.MissionState;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MissionListGetRequestParams {
+   private MissionState state;
+   private MissionCategory category;
+   private Long child;
 }
