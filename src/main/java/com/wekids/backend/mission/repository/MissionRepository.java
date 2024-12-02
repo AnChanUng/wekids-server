26dd3603becc15f7c305643a06ae3318d1fe4b91
YE-EUN <musiclove529@naver.com>
2024-12-02 16:00:57 +0900

    [feat/#59]: 미션 리스트 조회 쿼리 추가

--- src/main/java/com/wekids/backend/mission/repository/MissionRepository.java
+++ src/main/java/com/wekids/backend/mission/repository/MissionRepository.java
@@ -1,7 +1,16 @@
 package com.wekids.backend.mission.repository;
 
+import com.wekids.backend.member.domain.Child;
+import com.wekids.backend.member.domain.Parent;
 import com.wekids.backend.mission.domain.Mission;
+import com.wekids.backend.mission.domain.enums.MissionCategory;
+import com.wekids.backend.mission.domain.enums.MissionState;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
+    @Query("select m from Mission m join fetch m.child c where (:state is null or m.state = :state) and (:category is null or m.category = :category) and (:parent is null or m.parent = :parent) and (:child is null or c = :child)")
+    List<Mission> findAllByCondition(MissionState state, MissionCategory category, Parent parent, Child child);
 }
