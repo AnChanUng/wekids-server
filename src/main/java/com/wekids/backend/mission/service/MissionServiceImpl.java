b7e0bba5392c55ba7d7ffdb93989f32cb54b5a7d
YE-EUN <musiclove529@naver.com>
2024-12-02 14:25:27 +0900

    [feat/#58]: 미션 등록 서비스 코드 구현

--- src/main/java/com/wekids/backend/mission/service/MissionServiceImpl.java
+++ src/main/java/com/wekids/backend/mission/service/MissionServiceImpl.java
@@ -1,5 +1,14 @@
 package com.wekids.backend.mission.service;
 
+import com.wekids.backend.exception.ErrorCode;
+import com.wekids.backend.exception.WekidsException;
+import com.wekids.backend.member.domain.Child;
+import com.wekids.backend.member.domain.Parent;
+import com.wekids.backend.member.domain.mapping.ParentChildId;
+import com.wekids.backend.member.repository.ChildRepository;
+import com.wekids.backend.member.repository.ParentChildRepository;
+import com.wekids.backend.member.repository.ParentRepository;
+import com.wekids.backend.mission.domain.Mission;
 import com.wekids.backend.mission.dto.request.MissionCreateRequest;
 import com.wekids.backend.mission.dto.request.MissionListGetRequestParams;
 import com.wekids.backend.mission.dto.response.MissionGetResponse;
@@ -15,10 +24,34 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MissionServiceImpl implements MissionService{
     private final MissionRepository missionRepository;
+    private final ParentRepository parentRepository;
+    private final ChildRepository childRepository;
+    private final ParentChildRepository parentChildRepository;
 
     @Override
+    @Transactional
     public void createMission(MissionCreateRequest request, Long memberId) {
+        Parent parent = getParent(memberId);
+        Child child = getChild(request.getChildId());
 
+        validateMemberRelationship(parent.getId(), child.getId());
+
+        Mission mission = Mission.createNewMission(request, parent, child);
+        missionRepository.save(mission);
+    }
+
+    private void validateMemberRelationship(Long parentId, Long childId) {
+        ParentChildId parentChildId = ParentChildId.of(parentId, childId);
+        boolean checkRelationship = parentChildRepository.existsById(parentChildId);
+        if(!checkRelationship) throw new WekidsException(ErrorCode.INVALID_MEMBER_RELATIONSHIP, "부모 회원 아이디: " + parentId + " 자식 회원 아이디: " + childId);
+    }
+
+    private Child getChild(Long childId) {
+        return childRepository.findById(childId).orElseThrow(() -> new WekidsException(ErrorCode.MEMBER_NOT_FOUND, "자식 회원 아이디: " + childId));
+    }
+
+    private Parent getParent(Long parentId) {
+        return parentRepository.findById(parentId).orElseThrow(() -> new WekidsException(ErrorCode.MEMBER_NOT_FOUND, "부모 회원 아이디: " + parentId));
     }
 
     @Override
