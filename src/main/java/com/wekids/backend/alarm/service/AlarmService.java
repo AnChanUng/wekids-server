58c6c9459864a21f6ba9b66650fa55975262e7b9
YE-EUN <musiclove529@naver.com>
2024-12-04 13:25:10 +0900

    [feat/#64]: 알림 리스트 조회 서비스 코드 구현

--- src/main/java/com/wekids/backend/alarm/service/AlarmService.java
+++ src/main/java/com/wekids/backend/alarm/service/AlarmService.java
@@ -1,4 +1,9 @@
 package com.wekids.backend.alarm.service;
 
+import com.wekids.backend.alarm.dto.response.AlarmGetResponse;
+
+import java.util.List;
+
 public interface AlarmService {
+    List<AlarmGetResponse> getAlarmList(Long memberId);
 }
--- src/main/java/com/wekids/backend/alarm/service/AlarmServiceImpl.java
+++ src/main/java/com/wekids/backend/alarm/service/AlarmServiceImpl.java
@@ -1,10 +1,18 @@
 package com.wekids.backend.alarm.service;
 
+import com.wekids.backend.alarm.domain.Alarm;
+import com.wekids.backend.alarm.dto.response.AlarmGetResponse;
 import com.wekids.backend.alarm.repository.AlarmRepository;
+import com.wekids.backend.exception.ErrorCode;
+import com.wekids.backend.exception.WekidsException;
+import com.wekids.backend.member.domain.Member;
+import com.wekids.backend.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -12,4 +20,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class AlarmServiceImpl implements AlarmService{
     private final AlarmRepository alarmRepository;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public List<AlarmGetResponse> getAlarmList(Long memberId) {
+        Member member = getMember(memberId);
+        List<Alarm> alarms = alarmRepository.findAllByMember(member);
+        return AlarmGetResponse.from(alarms);
+    }
+
+    private Member getMember(Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(() -> new WekidsException(ErrorCode.MEMBER_NOT_FOUND, "회원 아이디: " + memberId));
+    }
 }
