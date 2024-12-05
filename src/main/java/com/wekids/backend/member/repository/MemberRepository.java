b9dee16c3f1a4bee0b0b64fc9f79406d8816825e
Kang Hyun Woo <gusdn741021@gmail.com>
2024-12-05 12:04:47 +0900

    [feat/#79]: 고객 정보 조회

--- src/main/java/com/wekids/backend/member/repository/MemberRepository.java
+++ src/main/java/com/wekids/backend/member/repository/MemberRepository.java
@@ -2,10 +2,14 @@ package com.wekids.backend.member.repository;
 
 import com.wekids.backend.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
     boolean existsAllByEmail(String email);
+    Optional<Member> findByNameAndPhone(String name, String phone);
 }
