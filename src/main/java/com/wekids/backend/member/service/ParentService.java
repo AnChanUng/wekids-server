960c808f89d19dfa06cca1f714b6c05b5fd0083b
Kang Hyun Woo <gusdn741021@gmail.com>
2024-12-05 15:26:19 +0900

    [feat/#80]: 부모 간편 비밀번호 변경

--- src/main/java/com/wekids/backend/member/service/ParentService.java
+++ src/main/java/com/wekids/backend/member/service/ParentService.java
@@ -4,10 +4,12 @@ import com.wekids.backend.admin.dto.request.MemberSimplePasswordRequest;
 import com.wekids.backend.member.dto.request.AccountInquiryAgreeRequest;
 import com.wekids.backend.member.dto.request.ParentAccountRegistrationRequest;
 import com.wekids.backend.member.dto.response.ParentAccountResponse;
+import com.wekids.backend.member.dto.response.ParentSimplePasswordRequest;
 
 public interface ParentService {
     ParentAccountResponse showParentAccount(Long parentId);
     void agreeAccountInquiry(AccountInquiryAgreeRequest accountInquiryAgreeRequest, Long memberId);
     void registerParentAccount(ParentAccountRegistrationRequest request, Long memberId);
-    void chageSimeplePassword(Long memberId, MemberSimplePasswordRequest request);
+    void changeSimplePassword(Long memberId, MemberSimplePasswordRequest request);
+    void changeSimplePassword(Long memberId, ParentSimplePasswordRequest request);
 }
