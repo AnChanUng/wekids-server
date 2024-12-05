99d418e7b79d185020402b4d9af2894171a64966
Kang Hyun Woo <gusdn741021@gmail.com>
2024-12-05 15:02:16 +0900

    [feat/#80]: 회원 정보 조회(마이페이지)

--- src/main/java/com/wekids/backend/member/service/MemberService.java
+++ src/main/java/com/wekids/backend/member/service/MemberService.java
@@ -2,7 +2,9 @@ package com.wekids.backend.member.service;
 
 
 import com.wekids.backend.admin.dto.request.MemberStateRequest;
+import com.wekids.backend.member.dto.response.MyPageResponse;
 
 public interface MemberService {
     public void updateMemberState(Long memberId, MemberStateRequest request);
+    public MyPageResponse showMyPage(Long memberId);
 }
--- src/main/java/com/wekids/backend/member/service/MemberServiceImpl.java
+++ src/main/java/com/wekids/backend/member/service/MemberServiceImpl.java
@@ -4,6 +4,7 @@ import com.wekids.backend.admin.dto.request.MemberStateRequest;
 import com.wekids.backend.exception.ErrorCode;
 import com.wekids.backend.exception.WekidsException;
 import com.wekids.backend.member.domain.Member;
+import com.wekids.backend.member.dto.response.MyPageResponse;
 import com.wekids.backend.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,12 @@ public class MemberServiceImpl implements MemberService{
     public void updateMemberState(Long memberId, MemberStateRequest request) {
         Member member = findMemberByMemberId(memberId);
         member.updateState(request.getState());
+    }
+
+    @Override
+    public MyPageResponse showMyPage(Long memberId) {
+        Member member = findMemberByMemberId(memberId);
+        return MyPageResponse.from(member);
     }
 
     public Member findMemberByMemberId(Long memberId){
