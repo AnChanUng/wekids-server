261da1bb7cebb9023b097055488b4cfed428f5bb
Kang Hyun Woo <gusdn741021@gmail.com>
2024-12-05 12:05:04 +0900

    [feat/#79]: 고객 정보 조회 controller

--- src/main/java/com/wekids/backend/admin/controller/AdminController.java
+++ src/main/java/com/wekids/backend/admin/controller/AdminController.java
@@ -1,13 +1,12 @@
 package com.wekids.backend.admin.controller;
 
+import com.wekids.backend.admin.dto.response.MemberInfoResponse;
+import com.wekids.backend.admin.service.AdminService;
 import com.wekids.backend.log.dto.reqeust.LogRequestParams;
 import com.wekids.backend.log.service.LogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -16,11 +15,17 @@ import java.util.Map;
 @RequestMapping("/admin")
 public class AdminController {
     private final LogService logService;
+    private final AdminService adminService;
 
     @GetMapping("/logs")
     public ResponseEntity<Map<String, String>> getLogs(@ModelAttribute LogRequestParams requestParams) {
         return ResponseEntity.ok(logService.showLogs(requestParams));
     }
 
-    
+    @GetMapping("/members")
+    public ResponseEntity<MemberInfoResponse> getMember(@RequestParam("name") String name, @RequestParam("phone") String phone) {
+        MemberInfoResponse memberInfoResponse = adminService.showMember(name, phone);
+        return ResponseEntity.ok(memberInfoResponse);
+    }
+
 }
