74fd257a494b2077cab46f1de7a934780efa9d6d
Kang Hyun Woo <gusdn741021@gmail.com>
2024-12-05 09:52:54 +0900

    [refact/#79]: admin controller로 책임 분리

--- src/main/java/com/wekids/backend/admin/controller/AdminController.java
+++ src/main/java/com/wekids/backend/admin/controller/AdminController.java
@@ -1,21 +1,26 @@
-package com.wekids.backend.log.controller;
+package com.wekids.backend.admin.controller;
 
 import com.wekids.backend.log.dto.reqeust.LogRequestParams;
 import com.wekids.backend.log.service.LogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/admin/logs")
-public class LogController {
+@RequestMapping("/admin")
+public class AdminController {
     private final LogService logService;
 
-    @GetMapping
+    @GetMapping("/logs")
     public ResponseEntity<Map<String, String>> getLogs(@ModelAttribute LogRequestParams requestParams) {
         return ResponseEntity.ok(logService.showLogs(requestParams));
     }
+
+    
 }
