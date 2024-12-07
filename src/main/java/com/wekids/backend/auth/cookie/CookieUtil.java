8e272fbf765985588de225b5ad4a3c7a76a2d6c3
Kang Hyun Woo <gusdn741021@gmail.com>
2024-12-07 09:34:26 +0900

    [ci]: 배포 테스트

--- src/main/java/com/wekids/backend/auth/cookie/CookieUtil.java
+++ src/main/java/com/wekids/backend/auth/cookie/CookieUtil.java
@@ -1,11 +1,15 @@
 package com.wekids.backend.auth.cookie;
 
 
+import com.wekids.backend.exception.ErrorCode;
+import com.wekids.backend.exception.WekidsException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseCookie;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 public class CookieUtil {
@@ -24,14 +28,21 @@ public class CookieUtil {
     }
 
     public static void addCookie(HttpServletResponse response, String name, String value) {
-        ResponseCookie cookie = ResponseCookie.from(name, value)
-                .path("/")
-                .sameSite("None")
-                .httpOnly(true)
-                .secure(true)
-                .maxAge(60 * 60 * 60)
-                .build();
 
-        response.addHeader("Set-Cookie", cookie.toString());
+        try{
+            String encodedValue = URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
+            ResponseCookie cookie = ResponseCookie.from(name, encodedValue)
+                    .path("/")
+                    .sameSite("None")
+                    .httpOnly(true)
+                    .secure(true)
+                    .maxAge(60 * 60 * 60)
+                    .build();
+
+            response.addHeader("Set-Cookie", cookie.toString());
+         }
+        catch (Exception e){
+            throw new WekidsException(ErrorCode.FAILED_COOKIE_ENCODING, "쿠키 인코딩 에러");
+         }
     }
 }
